474b5c143f70d842029041bbf0e6442352bc88ec
TomiiPorrini <tomas63025@gmail.com>
2023-05-17 10:29:03 -0300

    model Autor listo y registrado en el admin

--- Biblioteca/models.py
+++ Biblioteca/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    nacionalidad = models.CharField(max_length=50)
+    activo = models.BooleanField(default=True)

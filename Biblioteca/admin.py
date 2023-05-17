a6c1d258d13904af1781eac494ecb16c0155a062
Oscar López <oal0425@gmail.com>
2023-05-17 19:20:31 -0300

    Registrado el modelo Libro en el Admin

--- Biblioteca/admin.py
+++ Biblioteca/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Autor
+# Register your models here.
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    """ Registrar la entidad Autor en el admin  """
+    list_display = ('id', 'nombre', 'apellido', 'nacionalidad', 'activo')
+    search_fields = ('nombre', 'apellido')
+    list_filter = ['activo']
+
+class PrestamoLibroAdmin(admin.PrestamoLibro):
+    """ Registrar la entidad PrestamoLibro en el admin. """
+    list_display = ('fecha_pestammos','fecha_devolucion','socio','empleado','libro')
+    search_fields = ('socio','libro','empleado')
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelLibro):
+    list_display = ('titulo', 'descripcion', 'isbn', 'autor', 'activo')
+    search_fields = ('autor')

c2fb4194b63026a4610a6436df8521955a9da426
TomiiPorrini <tomas63025@gmail.com>
2023-05-18 19:34:41 -0300

    creacion de modelos y registros de admin de ellos

--- Biblioteca/admin.py
+++ Biblioteca/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
-
+from .models import autor, libro, prestamoLibro, empleado, socio
 # Register your models here.
+
+@admin.register(autor)
+class AutorAdmin(admin.ModelAdmin):
+    """ Registrar la entidad Autor en el admin  """
+    list_display = ('id', 'nombre', 'apellido', 'nacionalidad', 'activo')
+    search_fields = ('nombre', 'apellido')
+    list_filter = ['activo', 'nacionalidad']
+    
+@admin.register(empleado)
+class EmpleadoAdmin(admin.ModelAdmin):
+    """ Registrar la entidad Empleado en el admin  """
+    list_display = ('id', 'nombre', 'apellido', 'numero_legajo', 'activo')
+    search_fields = ('nombre', 'apellido')
+    list_filter = ['activo']
+
+@admin.register(socio)
+class SocioAdmin(admin.ModelAdmin):
+    """ Registrar la entidad Socio en el admin  """
+    list_display = ('id', 'nombre', 'apellido', 'fecha_nacimiento', 'activo')
+    search_fields = ('nombre', 'apellido')
+    list_filter = ['activo']
+
+@admin.register(libro)
+class LibroAdmin(admin.ModelAdmin):
+    """ Registrar la entidad Libro en el admin  """
+    list_display = ('id', 'titulo', 'descripcion', 'isbn', 'autor', 'activo')
+    search_fields = ('titulo',)
+    list_filter = ['activo']
+
+@admin.register(prestamoLibro)
+class PrestamoLibroAdmin(admin.ModelAdmin):
+    """ Registrar la entidad Prestamo de un libro en el admin. """
+    list_display = ('id', 'fecha_prestamos','fecha_devolucion','socio','empleado','libro')
+    search_fields = ('socio','libro','empleado')
--- Biblioteca/models.py
+++ Biblioteca/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class autor(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    nacionalidad = models.CharField(max_length=50)
+    activo = models.BooleanField(default=True)
+
+class libro(models.Model):
+    titulo = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=100)
+    isbn = models.IntegerField()
+    autor = models.ForeignKey(autor, on_delete=models.SET_NULL, null=True)
+    activo = models.BooleanField(default=True)
+
+class socio(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField()
+    activo = models.BooleanField()
+
+class empleado(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    numero_legajo = models.CharField(max_length=50)
+    activo = models.BooleanField(default=True)
+
+class prestamoLibro(models.Model):
+    fecha_prestamos = models.DateField(auto_now_add=True)
+    fecha_devolucion = models.DateField(auto_now_add=True)
+    socio = models.ForeignKey(socio, on_delete=models.SET_NULL, null=True)
+    empleado = models.ForeignKey(empleado, on_delete=models.SET_NULL, null=True)
+    libro = models.ForeignKey(libro, on_delete=models.SET_NULL, null=True)

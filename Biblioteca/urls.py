992eb664e6fb95ac44cff25f9a620b0ce129f3a5
TomiiPorrini <tomas63025@gmail.com>
2023-05-19 19:48:17 -0300

    correccion de path

--- Biblioteca/urls.py
+++ Biblioteca/urls.py
@@ -3,7 +3,7 @@ from Biblioteca import views
 
 
 urlpatterns = [
-    path("empleado/nuevo/", views.crear_empleado, name='crear-empleado'),
+    path("empleados/nuevo/", views.crear_empleado, name='crear-empleado'),
     path("empleados/modificar/<int:id>", views.modificar_empleado, name='modificar-empleado')
     
 ]

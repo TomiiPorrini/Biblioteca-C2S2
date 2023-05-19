65fcce1ad262a53c52530f717b7f7ec4901e61de
TomiiPorrini <tomas63025@gmail.com>
2023-05-19 20:00:09 -0300

    Corrigiendo mayusculas de los modelos y admin

--- Biblioteca/models.py
+++ Biblioteca/models.py
@@ -1,34 +1,34 @@
 from django.db import models
 
 # Create your models here.
-class autor(models.Model):
+class Autor(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     nacionalidad = models.CharField(max_length=50)
     activo = models.BooleanField(default=True)
 
-class libro(models.Model):
+class Libro(models.Model):
     titulo = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=100)
     isbn = models.IntegerField()
-    autor = models.ForeignKey(autor, on_delete=models.SET_NULL, null=True)
+    autor = models.ForeignKey(Autor, on_delete=models.SET_NULL, null=True)
     activo = models.BooleanField(default=True)
 
-class socio(models.Model):
+class Socio(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     fecha_nacimiento = models.DateField()
     activo = models.BooleanField()
 
-class empleado(models.Model):
+class Empleado(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     numero_legajo = models.CharField(max_length=50)
     activo = models.BooleanField(default=True)
 
-class prestamoLibro(models.Model):
+class PrestamoLibro(models.Model):
     fecha_prestamos = models.DateField(auto_now_add=True)
     fecha_devolucion = models.DateField(auto_now_add=True)
-    socio = models.ForeignKey(socio, on_delete=models.SET_NULL, null=True)
-    empleado = models.ForeignKey(empleado, on_delete=models.SET_NULL, null=True)
-    libro = models.ForeignKey(libro, on_delete=models.SET_NULL, null=True)
+    socio = models.ForeignKey(Socio, on_delete=models.SET_NULL, null=True)
+    empleado = models.ForeignKey(Empleado, on_delete=models.SET_NULL, null=True)
+    libro = models.ForeignKey(Libro, on_delete=models.SET_NULL, null=True)

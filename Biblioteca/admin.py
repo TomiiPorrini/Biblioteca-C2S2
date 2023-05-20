65f25f4b7f00db8a7b114b66737252b1e09c5da3
TomiiPorrini <tomas63025@gmail.com>
2023-05-20 09:38:46 -0300

    actualizacion de mayusculas en clases

--- Biblioteca/admin.py
+++ Biblioteca/admin.py
@@ -1,36 +1,36 @@
 from django.contrib import admin
-from .models import autor, libro, prestamoLibro, empleado, socio
+from .models import Autor, Libro, PrestamoLibro, Empleado, Socio
 # Register your models here.
 
-@admin.register(autor)
+@admin.register(Autor)
 class AutorAdmin(admin.ModelAdmin):
     """ Registrar la entidad Autor en el admin  """
     list_display = ('id', 'nombre', 'apellido', 'nacionalidad', 'activo')
     search_fields = ('nombre', 'apellido')
     list_filter = ['activo', 'nacionalidad']
     
-@admin.register(empleado)
+@admin.register(Empleado)
 class EmpleadoAdmin(admin.ModelAdmin):
     """ Registrar la entidad Empleado en el admin  """
     list_display = ('id', 'nombre', 'apellido', 'numero_legajo', 'activo')
     search_fields = ('nombre', 'apellido')
     list_filter = ['activo']
 
-@admin.register(socio)
+@admin.register(Socio)
 class SocioAdmin(admin.ModelAdmin):
     """ Registrar la entidad Socio en el admin  """
     list_display = ('id', 'nombre', 'apellido', 'fecha_nacimiento', 'activo')
     search_fields = ('nombre', 'apellido')
     list_filter = ['activo']
 
-@admin.register(libro)
+@admin.register(Libro)
 class LibroAdmin(admin.ModelAdmin):
     """ Registrar la entidad Libro en el admin  """
     list_display = ('id', 'titulo', 'descripcion', 'isbn', 'autor', 'activo')
     search_fields = ('titulo',)
     list_filter = ['activo']
 
-@admin.register(prestamoLibro)
+@admin.register(PrestamoLibro)
 class PrestamoLibroAdmin(admin.ModelAdmin):
     """ Registrar la entidad Prestamo de un libro en el admin. """
     list_display = ('id', 'fecha_prestamos','fecha_devolucion','socio','empleado','libro')

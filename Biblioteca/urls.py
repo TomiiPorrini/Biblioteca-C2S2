c9a51117de477e5fe15e8f4752f1cc1e1406512f
Carlos <carlosmorales1908@gmail.com>
2023-05-23 17:29:12 -0300

    creacion del formulario para actualizar un autor

--- Biblioteca/urls.py
+++ Biblioteca/urls.py
@@ -5,6 +5,6 @@ from Biblioteca import views
 urlpatterns = [
     path('empleados/activar/<int:id>', views.activar_empleado_view),
     path("empleados/nuevo/", views.crear_empleado, name='crear-empleado'),
-    path("empleados/modificar/<int:id>", views.modificar_empleado, name='modificar-empleado')
+    path("empleados/modificar/<int:id>", views.modificar_empleado, name='modificar-empleado'),
     path('empleado/listado', views.empleados)
 ]

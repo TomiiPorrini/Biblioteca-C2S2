9b27923c07cefbcb3053e544814b0f054a0b5b12
TomiiPorrini <tomas63025@gmail.com>
2023-06-05 21:07:18 -0300

    Documentacion de API

--- api/api.py
+++ api/api.py
@@ -5,21 +5,25 @@ from Biblioteca.models import Libro, Socio, Empleado, Autor
 # Create your views here.
 
 class LibroViewSet(viewsets.ModelViewSet):
+    """Tomará todos los datos de la bdd en el queryset y se serializa la data de los libros"""
     queryset = Libro.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = LibroSerializer
 
 class SocioViewSet(viewsets.ModelViewSet):
+    """Tomará todos los datos de la bdd en el queryset y se serializa la data de los socios"""
     queryset = Socio.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = SocioSerializer
     
 class EmpleadoViewSet(viewsets.ModelViewSet):
+    """Tomará todos los datos de la bdd en el queryset y se serializa la data de los empleados"""
     queryset = Empleado.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = EmpleadoSerializer
     
 class AutorViewSet(viewsets.ModelViewSet):
+    """Tomará todos los datos de la bdd en el queryset y se serializa la data de los autores"""
     queryset = Autor.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = AutorSerializer
--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,14 @@
 from rest_framework import routers
 from .api import LibroViewSet, SocioViewSet, EmpleadoViewSet, AutorViewSet
 
+#Se crea el router que nos va a crear las urls de cada viewset
 router = routers.DefaultRouter()
 
+#Registramos cada viewset a nuestro router y les ponemos un nombre 
 router.register('libros', LibroViewSet, 'api-libros')
 router.register('socios', SocioViewSet, 'api-socios')
 router.register('empleados', EmpleadoViewSet, 'api-empleados')
 router.register('autores', AutorViewSet, 'api-autores')
 
-#exportando las urls.
+#Exportamos las urls al urls.py principal del proyecto.
 urlpatterns = router.urls
